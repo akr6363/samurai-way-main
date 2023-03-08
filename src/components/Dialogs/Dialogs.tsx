633e5c4f2bccbc842e8d399d9b859019bb67b06c
Anna Krasilnikova <akr6363@mail.ru>
2023-03-08 20:41:08 +0300

    move data in BLL and use map

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,22 +2,48 @@ import React from 'react';
 import styles from './Dialogs.module.css'
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
+import {message} from "antd";
 
+type DialogsType = {
+    id: number
+    name: string
+}
+
+type MessageType = {
+    id: number
+    message: string
+}
 
 const Dialogs = () => {
+
+    let dialogsData: Array<DialogsType> = [
+        {id: 1, name: "Sveta"},
+        {id: 2, name: "Dima"},
+        {id: 3, name: "Igor"},
+        {id: 4, name: "Oksana"},
+        {id: 5, name: "Andrey"},
+    ]
+
+    let messageData: Array<MessageType> = [
+        {id: 1, message: "Hi"},
+        {id: 2, message: "How are you?"},
+        {id: 3, message: "Im fine motherfuker"},
+    ]
+
+    let dialogsElements = dialogsData
+        .map(dialog => <DialogItem key={dialog.id} name={dialog.name} id={dialog.id}/>)
+
+    let messagesElements = messageData
+        .map(message => <Message key={message.id} message={message.message}/>)
+
+
     return (
         <div className={styles.page}>
             <div className={styles.page__dialogs}>
-                <DialogItem name="Sveta" id={1}/>
-                <DialogItem name="Dima" id={2}/>
-                <DialogItem name="Igor" id={3}/>
-                <DialogItem name="Oksana" id={4}/>
-                <DialogItem name="Andrey" id={5}/>
+                {dialogsElements}
             </div>
             <div className={styles.page__messages}>
-                <Message message="Hi"/>
-                <Message message="How are you?"/>
-                <Message message="Im fine motherfuker"/>
+                {messagesElements}
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,16 +2,29 @@ import React from 'react';
 import styles from './MyPosts.module.css'
 import Post from "./Post/Post";
 
+type PostsType = {
+    id: number
+    message: string
+    likeCount: number
+}
+
+let postsData: Array<PostsType> = [
+    {id: 1, message: "Hi, how are you?", likeCount: 15},
+    {id: 2, message: "It is my first post", likeCount: 20},
+]
+
+let postsElements = postsData
+    .map(post => <Post key={post.id} message={post.message} likeCount={post.likeCount}/>)
+
 const MyPosts = () => {
     return (
-            <div className={styles.posts}>
-                <h2>My Posts</h2>
-                <textarea placeholder='your news'></textarea>
-                <button>Send</button>
-                <Post message = "Hi, how are you?" likeCount={15}/>
-                <Post message = "It is my first post" likeCount={20}/>
+        <div className={styles.posts}>
+            <h2>My Posts</h2>
+            <textarea placeholder='your news'></textarea>
+            <button>Send</button>
+            {postsElements}
 
-            </div>
+        </div>
     );
 };
 
